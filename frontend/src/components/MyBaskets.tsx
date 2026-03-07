6f846605b21940c762d840d40128d8f46629a122
jonnorrie <jonathontirabassi@gmail.com>
2026-03-07 18:18:53 -0500

    added iterations of received baskets to MyBasket component

--- frontend/src/components/MyBaskets.tsx
+++ frontend/src/components/MyBaskets.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const MyBaskets = () => {
+const MyBaskets = (props:any) => {
   return (
     <div>
       <h2>My Baskets</h2>
+      {props.baskets.map((basket: any) => {
+        return <p>{basket}</p>
+      })}
     </div>
   )
 }
