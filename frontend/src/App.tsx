0567b016cfd64c36af0e121c815e99f3f2b81fcc
jonnorrie <jonathontirabassi@gmail.com>
2026-03-07 16:11:03 -0500

    added MyBaskets component to App component, and styles for components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NewBasket from './components/NewBasket'
+import MyBaskets from './components/MyBaskets'
 
 function App() {
   return (
     <div className="App">
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>*/}
-      <NewBasket />
+      <div className="component-container">
+        <div className="new-basket-panel">
+          <NewBasket />
+        </div>
+        <div className="my-baskets-panel">
+          <MyBaskets />
+        </div>
+      </div>
     </div>
   );
 }
